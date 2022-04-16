701d4cb900eac3318ae23a351b241dcd3479fe47
Eunsung Moon <eunsung9647@gmail.com>
2022-04-16 15:41:05 +0900

    favicon 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Login from './pages/Login';
 import ReducerSample from './pages/ReducerSample';
-import { SampleProvider, useSampleState } from './context/SampleContext';
+import { useSampleState } from './context/SampleContext';
 
 function App() {
   const state = useSampleState();
-  console.log(state)
   return (
     <div className="App">
         {state.login==='login' ? <Login /> : null}
