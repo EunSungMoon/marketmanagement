6a6f3e7a7060801414bb94bf37ae1a41d60406c5
Eunsung Moon <eunsung9647@gmail.com>
2022-04-18 21:52:53 +0900

    main

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import logo from '../styles/images/logo.png';
 
 export default function Main() {
-  return <div>main </div>;
+  const navigate = useNavigate();
+
+  return (
+    <main id="main" className="container flex">
+      <img src={logo} alt="채미왔소" />
+      <div className="flex">
+        <button type="button" className='button-main backColor-1stg' onClick={() => navigate('/')}>
+          전체 목록보기
+        </button>
+        <button type="button" className='button-main backColor-2ndg' onClick={() => navigate('/')}>
+          제품 등록하기
+        </button>
+      </div>
+    </main>
+  );
 }
